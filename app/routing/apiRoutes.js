2af059dfbf8b863deddafcf5647946b19673aa30
unknown <sarah.lopriore@gmail.com>
2018-10-26 12:57:10 -0400

    added to html page and setup server and first route

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var path = require("path");
+
+var router = express.Router();
+
+
+
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 var express = require("express");
-var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 7500;
+
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
 
