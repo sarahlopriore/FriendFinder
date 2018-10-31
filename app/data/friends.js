abb53fd484308e60c5296e808b5e3eaf3ad556d9
sarahlopriore <sarah.lopriore@gmail.com>
2018-10-31 13:48:25 -0400

    add sample data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [{
-    "name": "dog",
-    "photo": "https://unsplash.com/photos/mx0DEnfYxic",
+    "name": "Dog",
+    "photo": "https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4800ae89e3ae4ae91bf6aa92c815d98e&auto=format&fit=crop&w=633&q=80",
     "scores": [
         5,
         5,
@@ -16,35 +16,147 @@ var friends = [{
 
 },
 {
-    "name": "goat",
-    "photo": "https://unsplash.com/photos/BkvRxV4tXEY",
+    "name": "Goat",
+    "photo": "https://images.unsplash.com/photo-1524609864400-829f6a124032?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=957c978d800f80bbdd91f5538ddf280e&auto=format&fit=crop&w=634&q=80",
     "scores": [
-        5,
-        4,
         2,
-        3,
+        4,
         1,
         2,
+        4,
+        3,
+        5,
+        4,
+        4,
+        2
+    ]  
+},
+{
+    "name": "Cat",
+    "photo": "https://images.unsplash.com/photo-1533738699159-d0c68059bb61?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0bb9888db1d270eab08c77f41120dfca&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+        2,
+        1,
         5,
         5,
         3,
+        4,
+        5,
+        5,
+        1,
         1
     ]  
 },
 {
-    "name": "cat",
-    "photo": "https://images.unsplash.com/photo-1533738699159-d0c68059bb61?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0bb9888db1d270eab08c77f41120dfca&auto=format&fit=crop&w=634&q=80",
+    "name": "Horse",
+    "photo": "https://images.unsplash.com/photo-1536141442894-ed5987d76d05?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ee00c85967261be23c6e65b0de85e414&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+        1,
+        2,
+        1,
+        2,
+        3,
+        3,
+        2,
+        3,
+        3,
+        4
+    ]  
+},
+{
+    "name": "Rabbit",
+    "photo": "https://images.unsplash.com/photo-1535241749838-299277b6305f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a6953c6f70087b9e4de75a37cec9afe9&auto=format&fit=crop&w=684&q=80",
+    "scores": [
+        2,
+        4,
+        3,
+        5,
+        2,
+        2,
+        3,
+        1,
+        4,
+        2
+    ]  
+},
+{
+    "name": "Snake",
+    "photo": "https://images.unsplash.com/photo-1528158222524-d4d912d2e208?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2d1da507513555201a815ea7da325d9c&auto=format&fit=crop&w=1024&q=80",
     "scores": [
         1,
         1,
+        3,
+        2,
+        1,
         5,
+        2,
+        3,
+        1,
+        3
+    ]  
+},
+{
+    "name": "Fox",
+    "photo": "https://images.unsplash.com/photo-1516934024742-b461fba47600?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4d38c917838d6435f6f676b2a5875be1&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+        3,
+        5,
+        1,
+        1,
+        2,
+        2,
         5,
         3,
-        4,
+        1,
+        4
+    ]  
+},
+{
+    "name": "Owl",
+    "photo": "https://images.unsplash.com/photo-1516233758813-a38d024919c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=082a679c643048fc14e40049cb5bc7bb&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+        3,
+        3,
+        2,
+        1,
+        5,
         2,
         1,
         2,
+        2,
+        4
+    ]  
+},
+{
+    "name": "Frog",
+    "photo": "https://images.unsplash.com/photo-1506506447188-78e2a1051d9e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6f7806c0a01af0521ed3e8062ce137d2&auto=format&fit=crop&w=1053&q=80",
+    "scores": [
+        5,
+        5,
+        2,
+        5,
+        2,
+        4,
+        5,
+        4,
+        1,
         5
+    ]  
+},
+{
+    "name": "Deer",
+    "photo": "https://images.unsplash.com/photo-1476922027627-aa7293e3aaa8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6566c3a3c4703f7d7a8c2b0f96981b60&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+        1,
+        2,
+        3,
+        4,
+        2,
+        3,
+        1,
+        5,
+        1,
+        4
     ]  
 }]
 
