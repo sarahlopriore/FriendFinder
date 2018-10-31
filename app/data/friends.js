47c565a6b3d6335d9b027a6a4e5a7870154166a6
sarahlopriore <sarah.lopriore@gmail.com>
2018-10-30 22:56:30 -0400

    find match

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friends = [{
+    "name": "dog",
+    "photo": "https://unsplash.com/photos/mx0DEnfYxic",
+    "scores": [
+        5,
+        5,
+        1,
+        1,
+        5,
+        3,
+        4,
+        4,
+        5,
+        1
+    ]
+
+},
+{
+    "name": "goat",
+    "photo": "https://unsplash.com/photos/BkvRxV4tXEY",
+    "scores": [
+        5,
+        4,
+        2,
+        3,
+        1,
+        2,
+        5,
+        5,
+        3,
+        1
+    ]  
+},
+{
+    "name": "cat",
+    "photo": "https://images.unsplash.com/photo-1533738699159-d0c68059bb61?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0bb9888db1d270eab08c77f41120dfca&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+        1,
+        1,
+        5,
+        5,
+        3,
+        4,
+        2,
+        1,
+        2,
+        5
+    ]  
+}]
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,70 @@
 var express = require("express");
 var path = require("path");
 
+var friends = require("../data/friends");
+
 var router = express.Router();
 
+router.get("/api/friends", function(req, res){
+    res.json(friends);
+})
 
 
+router.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    console.log(newFriend);
 
+    var newScores = newFriend.scores;
+    newScores = newScores.map(Number);
+    // console.log(newScores);
+
+
+        var newTotal = 0;
+        newScores.forEach(function(x) {
+            newTotal += x;
+        })
+        console.log(newTotal);
+
+
+        var scoreArray = [];
+
+        for (i = 0; i < friends.length; i++) {
+            var userScores = friends[i].scores;
+            userScores = userScores.map(Number);
+            console.log(userScores);
+    
+            var userTotal = 0;
+            userScores.forEach(function(x) {
+                userTotal += x;
+            })
+            // console.log(userTotal);
+
+            scoreArray.push(userTotal);
+        }
+        console.log(scoreArray);
+
+        var closest = function(array, num) {
+            var i = 0;
+            var minDiff = 1000;
+            var ans;
+            for (i in array) {
+                var m = Math.abs(num - array[i]);
+                if (m < minDiff) {
+                    minDiff = m;
+                    ans = array[i];
+                }
+            }
+            return ans;
+        }
+
+        var answer = closest(scoreArray, newTotal);
+        console.log(answer);
+        var index = scoreArray.indexOf(answer);
+
+
+    friends.push(newFriend);
+    res.json(friends[index]);
+})
 
 
 
